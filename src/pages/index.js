7c19a27cfaefdabdc4291521f3cd994ff240e293
Ejike <gbuzueejiketh@gmail.com>
2024-10-05 09:34:13 +0100

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,36 @@
 import { ClientPageRoot } from "next/dist/client/components/client-page";
-import localFont from "next/font/local";
+import React from "react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-const a = 100;
-const b = 150;
+const ArrowFunction = () => {
+  return (
+    <>
+      <h1 className="font-bold bg-blue-500">I am arrow function</h1>
+    </>
+  );
+};
 function CompA() {
   return (
     <div>
+      <ArrowFunction />
       <h1 className="font-bold">CompA</h1>
       <p>This is CompA</p>
-      <CompB />
     </div>
   );
 }
-
-function CompB() {
-  return (
-    <div>
-      <h1 className="font-bold">CompB</h1>
-      <p>This is CompB</p>
-    </div>
-  );
+class CompC extends React.Component {
+  render() {
+    return <h1 className="font-bold">CompC</h1>;
+  }
 }
-
-export default function Home() {
+export default function () {
   return (
-    <div>
-      <h1 className="font-bold underline">
+    <>
+      <h1 className="font-bold underline text-3xl">
         Hello World, This is a comeback to NextJS his comes with an extensive
         design plan
       </h1>
       <CompA />
-    </div>
+      {/* <CompC /> */}
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin"); // Add this line
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
